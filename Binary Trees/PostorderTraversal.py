f7c1fb181492f06ce86002f6e55ba341256a2db0
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-22 10:28:19 +0530

    binary tree postorder traversak

---
+++ Binary Trees/PostorderTraversal.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self,val):
+        self.val=val
+        self.left=None
+        self.right=None
+drinks=Node('drinks')
+hot=Node('hot')
+cold=Node('cold')
+tea=Node('tea')
+coffe=Node('coffe')
+cola=Node('cola')
+sprite=Node('sprite')
+drinks.left=hot
+drinks.right=cold
+hot.left=tea
+hot.right=coffe
+cold.left=cola
+cold.right=sprite
+
+# print(hot.left.val)
+def postorderTraversal(node):
+    if not node:
+        return
+    postorderTraversal(node.left)
+    postorderTraversal(node.right)
+    print(node.val)
+postorderTraversal(drinks)
