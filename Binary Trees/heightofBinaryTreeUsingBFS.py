7158404f0bdcc16464f5a7faea29c415677a7a4a
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-23 13:14:49 +0530

    height of a binary tree using BFS

---
+++ Binary Trees/heightofBinaryTreeUsingBFS.py
@@ -0,0 +1,35 @@
+from collections import deque
+class Node:
+    def __init__(self,val):
+        self.val=val
+        self.left=None
+        self.right=None
+drinks=Node('drinks')
+hot=Node('hot')
+cold=Node('cold')
+tea=Node('tea')
+coffe=Node('coffe')
+cola=Node('cola')
+sprite=Node('sprite')
+drinks.left=hot
+drinks.right=cold
+hot.left=tea
+hot.right=coffe
+cold.left=cola
+cold.right=sprite
+def heightNode(node):
+    result=[]
+    queue=deque([])
+    height=0
+    queue.append(node)
+    while len(queue)!=0:
+        levelSize=len(queue)
+        height+=1
+        for _ in range(levelSize):
+            e=queue.popleft()
+            if e.left is not None:
+                queue.append(e.left)
+            if e.right is not None:
+                queue.append(e.right)
+    return height
+print(heightNode(drinks))
